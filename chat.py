4490d4cb3a5e0f3103e8b83f63717fb2434a6343
arielsonalt <32400603+arielsonalt@users.noreply.github.com>
2018-04-30 23:25:16 -0300

    Update chat

--- chat.py
+++ chat.py
@@ -4,15 +4,15 @@ from chatterbot.trainers import ListTrainer  # trainer
 from chatterbot import ChatBot  # chatbot
 import os
 
-rafael = ChatBot('Test', read_only=True)
-rafael.set_trainer(ListTrainer)
+bot = ChatBot('Test', read_only=True)
+bot.set_trainer(ListTrainer)
 
 for arquivo in os.listdir('arquivo'):
     chats = open('arquivo/'+ arquivo, 'r').readlines()
-    rafael.train(chats)
+    bot.train(chats)
 
 while True:
     pergunta = input('Você: ')
 
-    resposta = rafael.get_response(pergunta)
+    resposta = bot.get_response(pergunta)
     print('Bot: ',resposta)
